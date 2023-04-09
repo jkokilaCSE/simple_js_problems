d76dd869179574d6d3a16e83b762462027f26283
jkokilaCSE <jkokila2810@gmail.com>
2023-04-09 20:59:28 +0530

    sample workouts

---
+++ age.js
@@ -0,0 +1,8 @@
+let age =21
+
+if(age<=20){
+    console.log("You can not enter the club!")
+}
+else{
+    console.log("welcome!")
+}
---
+++ purchase.js
@@ -0,0 +1,8 @@
+
+
+
+ let errorMsg=document.getElementById("error")
+  function errormsg(){
+    errorMsg.textContent="Something went wrong, please try again"
+
+  }
